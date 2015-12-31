f263c5b4358b2818663e81379e9e1082eb66c363
Bill Smartt <bsmartt@alienvault.com>
2015-12-31 08:30:18 -0800

    main.go - cleanup main

--- examples/main.go
+++ examples/main.go
@@ -12,18 +12,6 @@ import (
 )
 
 func main() {
-//	client := otxapi.NewClient(nil)
-//	fmt.Println(client)
-//	fmt.Println("Recently updated repositories owned by user willnorris:")
-////
-//	otxapi.SubscriptionList()
-//	opt := &otxapi.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
-//	repos, _, err := client.Repositories.List("willnorris", opt)
-//	if err != nil {
-//		fmt.Printf("error: %v\n\n", err)
-//	} else {
-//		fmt.Printf("%v\n\n", github.Stringify(repos))
-//	}
 
     var PulsesStruct otxapi.SubscribedPulses
 	p := PulsesStruct.List()
