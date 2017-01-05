9d21e607d5c3b8424d4547736f30bd51d56d9fb6
Nick Saika <nicksaika@gmail.com>
2017-01-05 16:36:57 -0500

    otxapi: move pulse and user service-related code into their own files
    This way, the type definitions, and the methods on those types are
    closer together.

--- src/otxapi/pulses.go
+++ src/otxapi/pulses.go
@@ -1,51 +1,232 @@
 package otxapi
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
 
+	"github.com/google/go-querystring/query"
+)
 
 type OTXPulseDetailService struct {
 	client *Client
 }
 
-// Pulse represents an OTX Pulse
-type PulseDetail struct {
-	ID               *string                 `json:"id"`
-	Author           *string                 `json:"author_name"`
-	Name             *string                 `json:"name"`
-	Description      *string                 `json:"description,omitempty"`
-	CreatedAt        *string                 `json:"created,omitempty"`
-	ModifiedAt       *string                 `json:"modified"`
-	References       []string                `json:"references,omitempty"`
-	Tags             []string                `json:"tags,omitempty"`
-	Indicators       []struct {
-		ID               *string                 `json:"_id"`
-		Indicator        *string                 `json:"indicator"`
-		Type             *string                 `json:"type"`
-		Description      *string                 `json:"description,omitempty"`
-	}                                        `json:"indicators,omitempty"`
-	Revision         *float32                `json:"revision,omitempty"`
+// Get returns a single *Pulse.
+func (svc *OTXPulseDetailService) Get(pulseID string) (*Pulse, error) {
+	req, err := svc.client.newRequest(http.MethodGet, PulseDetailURLPath+pulseID, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var p Pulse
+	if err := svc.client.do(req, &p); err != nil {
+		return nil, err
+	}
+	return &p, nil
 }
 
-func (r PulseDetail) String() string {
+// Pulse represents an OTX Pulse.
+type Pulse struct {
+	ID                string           `json:"id"`
+	Author            string           `json:"author_name"`
+	Name              string           `json:"name"`
+	Description       string           `json:"description,omitempty"`
+	CreatedAt         string           `json:"created,omitempty"`
+	ModifiedAt        string           `json:"modified"`
+	References        []string         `json:"references,omitempty"`
+	Tags              []string         `json:"tags,omitempty"`
+	Indicators        []PulseIndicator `json:"indicators,omitempty"`
+	Revision          int              `json:"revision,omitempty"`
+	TLP               string           `json:"tlp"`
+	Public            bool             `json:"public"`
+	Adversary         string           `json:"adversary"`
+	TargetedCountries []string         `json:"targeted_countries"`
+	Industries        []string         `json:"industries"`
+}
+
+func (r Pulse) String() string {
 	return Stringify(r)
 }
 
-type OTXThreatIntelFeedService struct {
-	client *Client
+type PulseIndicator struct {
+	Content      string   `json:"content"`
+	Indicator    string   `json:"indicator"`
+	Description  string   `json:"description,omitempty"`
+	Created      string   `json:"created"`
+	Active       int      `json:"is_active"`
+	Title        string   `json:"title"`
+	AccessReason string   `json:"access_reason"`
+	AccessType   string   `json:"access_type"`
+	AccessGroups []string `json:"access_groups"`
+	Type         string   `json:"type"`
+	ID           string   `json:"id"`
+	Observations int      `json:"observations"`
+
+	// These fields are currently always nil, in responses.
+	Role       json.RawMessage `json:"role"`
+	Expiration json.RawMessage `json:"expiration"`
 }
 
-type ThreatIntelFeed struct {
-	Pulses      []PulseDetail     `json:"results"`
+// List returns a *PulseList, which maps to a single page of pulses the user
+// is subscribed to.
+func (svc *OTXPulseDetailService) List(opt *ListOptions) (*PulseList, error) {
+	req, err := svc.client.newRequest(http.MethodGet, SubscriptionsURLPath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := addURLOptions(req.URL, opt); err != nil {
+		return nil, fmt.Errorf("applying url options: %v", err)
+	}
+
+	var list PulseList
+	if err := svc.client.do(req, &list); err != nil {
+		return nil, err
+	}
+	return &list, nil
+}
+
+// PulseList represents a single page of results, where each page holds a list
+// of pulses the user has subscribed to.
+type PulseList struct {
+	Pulses []Pulse `json:"results"`
+
 	// These fields provide the page values for paginating through a set of
 	// results.  Any or all of these may be set to the zero value for
 	// responses that are not part of a paginated set, or for which there
 	// are no additional pages.
 	//NextPageNum  int   Coming soon
 	//PrevPageNum  int   Coming soon
-	NextPageString  *string	     `json:"next"`
-	PrevPageString  *string      `json:"prev"`
-	Count           int          `json:"count"`
+	NextPageString *string `json:"next"`
+	PrevPageString *string `json:"prev"`
+	Count          int     `json:"count"`
 }
 
-func (r ThreatIntelFeed) String() string {
+func (r PulseList) String() string {
 	return Stringify(r)
+}
+
+// ErrNoPage indicates that there is no "next" or "previous" page.
+var ErrNoPage = errors.New("no more pages")
+
+// NextPageOptions returns *ListOptions that can be used for retrieving the
+// next page of results.
+//
+// If the PulseList contains the last page of results, this method will
+// return ErrNoPage.
+func (r PulseList) NextPageOptions() (*ListOptions, error) {
+	if r.NextPageString == nil {
+		return nil, ErrNoPage
+	}
+	opts, err := parseListOptions(*r.NextPageString)
+	if err != nil {
+		return nil, err
+	}
+	return opts, nil
+}
+
+// PrevPageOptions returns *ListOptions that can be used for retrieving the
+// previous page of results.
+//
+// If the PulseList contains the first page of results, this method will return
+// ErrNoPage.
+func (r PulseList) PrevPageOptions() (*ListOptions, error) {
+	if r.PrevPageString == nil {
+		return nil, ErrNoPage
+	}
+	opts, err := parseListOptions(*r.PrevPageString)
+	if err != nil {
+		return nil, err
+	}
+	return opts, nil
+}
+
+// parseListOptions returns *ListOptions that have been parsed from the given
+// URL string.
+//
+// If the "?limit" URL query parameter is < 0, or > 20, then limit of results
+// per page will be set to 20.
+//
+// NOTE(nesv): 20 is currently the maximum number of results allowed, per page.
+func parseListOptions(urlStr string) (*ListOptions, error) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	page, err := strconv.Atoi(u.Query().Get("page"))
+	if err != nil {
+		return nil, err
+	}
+
+	limit, err := strconv.Atoi(u.Query().Get("limit"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Make sure we stay within page size limits.
+	if limit > 20 || limit < 0 {
+		limit = 20
+	}
+
+	return &ListOptions{
+		Page:    page,
+		PerPage: limit,
+	}, nil
+}
+
+// ListOptions specifies the optional parameters to various List methods that
+// support pagination.  our list options: ?limit=50&page_num=1
+type ListOptions struct {
+	// For paginated result sets, page of results to retrieve.
+	Page int `url:"page,omitempty"`
+
+	// For paginated result sets, the number of results to include per page.
+	// The maximum value is 20.
+	PerPage int `url:"limit,omitempty"`
+}
+
+// addOptions adds the parameters in opt as URL query parameters to s.  opt
+// must be a struct whose fields may contain "url" tags.
+func addOptions(s string, opt *ListOptions) (string, error) {
+	if opt == nil {
+		return s, nil
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", err
+	}
+
+	qs, err := query.Values(opt)
+	if err != nil {
+		return "", err
+	}
+
+	u.RawQuery = qs.Encode()
+	return u.String(), nil
+}
+
+// addURLOptions modifies the query parameters of u, in place.
+//
+// This function will return a non-nil error if u is nil.
+func addURLOptions(u *url.URL, opt *ListOptions) error {
+	if u == nil {
+		return errors.New("nil url")
+	}
+	if opt == nil {
+		return nil
+	}
+
+	qs, err := query.Values(opt)
+	if err != nil {
+		return err
+	}
+
+	u.RawQuery = qs.Encode()
+	return nil
 }
--- src/otxapi/users.go
+++ src/otxapi/users.go
@@ -1,14 +1,16 @@
 package otxapi
 
+import "net/http"
+
 type UserDetail struct {
-	AwardCount      *int                 `json:"award_count"`
-	FollowerCount   *int                 `json:"follower_count"`
-	SubscriberCount *int                 `json:"subscriber_count"`
-	IndicatorCount  *int                 `json:"indicator_count"`
-	PulseCount      *int                 `json:"pulse_count"`
-	MemberSince     *string              `json:"member_since"`
-	UserId          *int                 `json:"user_id"`
-	Username        *string              `json:"username"`
+	AwardCount      *int    `json:"award_count"`
+	FollowerCount   *int    `json:"follower_count"`
+	SubscriberCount *int    `json:"subscriber_count"`
+	IndicatorCount  *int    `json:"indicator_count"`
+	PulseCount      *int    `json:"pulse_count"`
+	MemberSince     *string `json:"member_since"`
+	UserId          *int    `json:"user_id"`
+	Username        *string `json:"username"`
 }
 
 func (r UserDetail) String() string {
@@ -19,3 +21,16 @@ type OTXUserDetailService struct {
 	client *Client
 }
 
+func (c *OTXUserDetailService) Get() (*UserDetail, error) {
+	req, err := c.client.newRequest(http.MethodGet, UserURLPath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var user UserDetail
+	if err := c.client.do(req, &user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
