f37ef546a7cef00faa5ef84d0ed08f2c12c28a89
Nick Saika <nicksaika@gmail.com>
2017-01-05 18:41:00 -0500

    added more godoc examples

--- src/otxapi/otxapi_test.go
+++ src/otxapi/otxapi_test.go
@@ -3,7 +3,16 @@ package otxapi
 import (
 	"fmt"
 	"log"
+	"net/http"
 )
+
+func ExampleAPIKey() {
+	client, err := NewClient(APIKey("..."))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(client)
+}
 
 func ExampleAPIKeyFromEnv_default() {
 	client, err := NewClient(APIKeyFromEnv())
@@ -19,6 +28,23 @@ func ExampleAPIKeyFromEnv_customVarNames() {
 		"OTX_KEY",
 		"OTX_API_KEY",
 	))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(client)
+}
+
+func ExampleHTTPClient() {
+	customClient := &http.Client{}
+	client, err := NewClient(HTTPClient(customClient))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(client)
+}
+
+func ExampleUserAgent() {
+	client, err := NewClient(UserAgent("custom-otx-api-client/13.37"))
 	if err != nil {
 		log.Fatalln(err)
 	}
